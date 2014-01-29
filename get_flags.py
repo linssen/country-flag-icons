2f68fc501352de20fce245345b6d1e1c184378e4
Wil Linssen <wil@linssen.me>
2014-01-29 11:50:18 +0000

    Full name is no use, we need the country code

--- get_flags.py
+++ get_flags.py
@@ -12,29 +12,33 @@ WIKI_URL = u'http://en.wikipedia.org'
 def main():
     r = requests.get('%s/wiki/ISO_3166-1' % WIKI_URL)
     soup = BeautifulSoup(r.text, 'html5lib')
-    country_urls = [a['href'] for a in soup.select('#mw-content-text table:nth-of-type(1) tr > td:nth-of-type(1) > a')]
-    for country_url in country_urls[]:
-        # print 'Looking for \'%s\'' % country_url
-        get_flag_url(country_url)
+    country_rows = soup.select('#mw-content-text table:nth-of-type(1) tr')
+    for row in country_rows[1:]:
+        get_flag_url(dict(
+            url=row.select('td:nth-of-type(1) a')[0]['href'],
+            alpha3=row.select('td:nth-of-type(3)')[0].get_text(),
+            name=row.select('td:nth-of-type(1')[0].get_text(),
+        ))
 
-def get_flag_url(country_url):
-    r = requests.get(WIKI_URL + country_url)
+def get_flag_url(country):
+    r = requests.get(WIKI_URL + country['url'])
     soup = BeautifulSoup(r.text, 'html5lib')
-    media_url = soup.find(title=re.compile('^Flag of'))['href']
-    # print 'Found media page: \'%s\'' % media_url
-
+    media_link = soup.find(title=re.compile('^Flag of'))
+    if media_link is None:
+        print 'No flag found for \'%s\'' % country['name']
+        return False
+    media_url = media_link['href']
     r = requests.get(WIKI_URL + media_url)
     soup = BeautifulSoup(r.text, 'html5lib')
-    file_url = soup.select('#file > a')[0]['href']
-    # print 'Found file: \'%s\'' % file_url
-    download_flag(file_url)
+    country['file_url'] = soup.select('#file > a')[0]['href']
+    download_flag(country)
 
-def download_flag(file_url):
-    file_name = os.path.basename(file_url)
+def download_flag(country):
+    file_name = os.path.basename('%s.svg' % country['alpha3'])
     file_name = urllib.unquote(file_name).decode('utf8').lower()
     path = os.path.join(os.path.dirname(__file__), 'images', file_name)
-    r = requests.get(WIKI_URL + file_url)
-    print 'Saving file: \'%s\'' % file_name
+    r = requests.get(WIKI_URL + country['file_url'])
+    print 'Saving file: \'%s\' for %s' % (file_name, country['name'])
     with open(path, 'wb') as f:
         for chunk in r.iter_content():
             f.write(chunk)
